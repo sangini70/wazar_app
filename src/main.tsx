aded8a5965a4730f37d142154346a97d616f3d79
rubeon <luganopizza@gmail.com>
2026-02-18 13:41:39 +0900

    Improve root element check before rendering App
    Refactor ReactDOM rendering to check for root element existence.

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
-// import "./index.css";  // ← 여기로 이동시켜야 합니다
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = document.getElementById("root");
 
+if (root) {
+  ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
