461c662202d15efa98cfd0e9c40fb3c24dec08b0
rubeon <luganopizza@gmail.com>
2026-02-18 14:49:33 +0900

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
-import App from './App';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
