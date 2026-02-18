e9ec03df7a8285c98f154bbb9b37a599a1622460
rubeon <luganopizza@gmail.com>
2026-02-18 13:28:11 +0900

    Reorganize index.css import position
    Moved the index.css import statement to a different location.

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+// import "./index.css";  // ← 여기로 이동시켜야 합니다
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-// import "./index.css";
 
