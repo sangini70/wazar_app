623125fe927d0b70d20a68a6d74f7da28b482aa6
rubeon <luganopizza@gmail.com>
2026-02-18 14:12:19 +0900

    Create App component with initial content

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+export default function App() {
+  return (
+    <div style={{ padding: 24, fontSize: 28, fontWeight: 800 }}>
+      WAZRA WATER
+      <div style={{ marginTop: 12, fontSize: 16, fontWeight: 400 }}>
+        React + Vite 정상 작동 확인
+      </div>
+    </div>
+  );
+}
