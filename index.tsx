31731d116d20184bf1d1cf64eafdd62e74b594cf
rubeon <luganopizza@gmail.com>
2026-02-18 14:46:21 +0900

    Fix import formatting in index.tsx

--- index.tsx
+++ index.tsx
@@ -1,4 +1,6 @@
-import * as React from 'react'; / import * as ReactDOM from 'react-dom/client';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import App from './App';
 import App from './App';
 
 const rootElement = document.getElementById('root');
